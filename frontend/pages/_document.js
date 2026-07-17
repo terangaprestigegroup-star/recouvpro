f12ec49169bfbde40ef8e87a425311cb9371a384
terangaprestigegroup-star <terangaprestigegroup@gmail.com>
2026-07-17 08:39:52 +0000

    Update meta tags and favicon in _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -9,9 +9,9 @@ export default function Document() {
         <meta name="referrer"                    content="strict-origin-when-cross-origin" />
         <meta name="mobile-web-app-capable"      content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title"  content="RecouvPro" />
-        <meta name="theme-color"                 content="#0D1B0F" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>💸</text></svg>" />
+        <meta name="apple-mobile-web-app-title"  content="Fluxio" />
+        <meta name="theme-color"                 content="#0A1A0C" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚡</text></svg>" />
       </Head>
       <body>
         <Main />
